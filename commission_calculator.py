ef6235bfb4262b4358b2712540efc3b549f009e9
Grifo55 <guillepcorbella@hotmail.com>
2022-06-01 17:00:53 +0200

    The definitive code. Changes to currency and ask it in a more polite way. Now the commission percentage is a variable and can be changed.

--- commission_calculator.py
+++ commission_calculator.py
@@ -1,6 +1,7 @@
 id = input("Please, introduce your ID? ")
 total_sales = float(input("How much do your sales amount to? "))
+comm_percentage = 13
 
-commission = round(total_sales * 13 / 100,2)
+commission = round(total_sales * comm_percentage / 100,2)
 
 print(f"{id} your commission this month ascend to {commission}$")
